4079b19731e6acb55c0d9bc5ea6e6cc96d0eb5b1
8or1and1 <8or1and1@gmail.com>
2022-05-03 15:02:14 +0300

    app ready

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	
+	@IBOutlet weak var cityLabel: UILabel!
 	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var temperatureLabel: UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -22,13 +23,14 @@ class ViewController: UIViewController {
 
 extension ViewController: UISearchBarDelegate{
 	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		searchBar.resignFirstResponder()
 		let urlString = "http://api.weatherstack.com/current?access_key=8b008068b581fb1a492b58c629ee25ed&query=\(searchBar.text!)"
 		let url = URL.init(string: urlString)
 		
 		var locationName: String?
 		var temperature: Int?
 		
-		let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+		let task = URLSession.shared.dataTask(with: url!) {[weak self] (data, response, error) in
 			do {
 				let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
 				if let location =  json["location"] {
@@ -37,7 +39,10 @@ extension ViewController: UISearchBarDelegate{
 				if let current =  json["current"] {
 					temperature = current["temperature"] as? Int
 				}
-				
+				DispatchQueue.main.async {
+					self?.cityLabel.text  = locationName
+					self?.temperatureLabel.text = temperature?.description
+				}
 			}
 			catch let jsonError {
 				print(jsonError)
