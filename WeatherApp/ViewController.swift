576d74b5ba8c457008904edc5304366e87e1c5d8
8or1and1 <8or1and1@gmail.com>
2022-05-03 14:23:53 +0300

    get values

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -24,8 +24,24 @@ extension ViewController: UISearchBarDelegate{
 	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
 		let urlString = "http://api.weatherstack.com/current?access_key=8b008068b581fb1a492b58c629ee25ed&query=\(searchBar.text!)"
 		let url = URL.init(string: urlString)
+		
+		var locationName: String?
+		var temperature: Int?
+		
 		let task = URLSession.shared.dataTask(with: url!) { data, response, error in
-			
+			do {
+				let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
+				if let location =  json["location"] {
+					locationName = location["name"] as? String
+				}
+				if let current =  json["current"] {
+					temperature = current["temperature"] as? Int
+				}
+				
+			}
+			catch let jsonError {
+				print(jsonError)
+			}
 		}
 		task.resume()
 	}
