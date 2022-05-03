23a6bad83460c23ff538e5dd9b12cf630cb165a7
8or1and1 <8or1and1@gmail.com>
2022-05-03 12:07:03 +0300

    Request done

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	@IBOutlet weak var searchBar: UISearchBar!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		searchBar.delegate = self
+		
 	}
-
 
 }
 
+extension ViewController: UISearchBarDelegate{
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		let urlString = "http://api.weatherstack.com/current?access_key=8b008068b581fb1a492b58c629ee25ed&query=\(searchBar.text!)"
+		let url = URL.init(string: urlString)
+		let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+			
+		}
+		task.resume()
+	}
+}
